f8f896c5502e60d30fed0e7e42a91c3912d40dc6
ArcadeFortune <lamaa@bzz.ch>
2023-05-11 08:46:10 +0200

    fix: Player moves towards the center of the click

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,34 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-function Player ({ clientX, clientY }) {
+function Player ({ clientX, clientY }) {  // Create a reference to the player element
+  const playerRef = useRef(null);
+
+  // Create a state variable for the player's position
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
+  // Set up an interval to update the player's position
   useEffect(() => {
+    // Get the player's width and height
+    const playerWidth = playerRef.current.offsetWidth;
+    const playerHeight = playerRef.current.offsetHeight;
+
+    // Set up the interval
     const interval = setInterval(() => {
       setPosition((prevPosition) => {
-        const distance = Math.sqrt(
-          Math.pow(clientX - prevPosition.x, 2) +
-            Math.pow(clientY - prevPosition.y, 2)
-        );
-        const speed = 5;
+        // Calculate the target position and distance to it
+        const { x: prevX, y: prevY } = prevPosition;
+        const { x: targetX, y: targetY } = { x: clientX - playerWidth / 2, y: clientY - playerHeight / 2 };
+        const distance = Math.hypot(targetX - prevX, targetY - prevY);
+
+        // Move the player towards the target position
+        const speed = 50;
         if (distance <= speed) {
-          return { x: clientX, y: clientY };
+          // If the player is close enough to the target, snap to it
+          return { x: targetX, y: targetY };
         } else {
-          const angle = Math.atan2(clientY - prevPosition.y, clientX - prevPosition.x);
-          const x = prevPosition.x + speed * Math.cos(angle);
-          const y = prevPosition.y + speed * Math.sin(angle);
+          // Otherwise, move the player towards the target
+          const angle = Math.atan2(targetY - prevY, targetX - prevX);
+          const x = prevX + speed * Math.cos(angle);
+          const y = prevY + speed * Math.sin(angle);
           return { x, y };
         }
       });
     }, 10);
 
+    // Clean up the interval on unmount
     return () => clearInterval(interval);
   }, [clientX, clientY]);
 
-
-
   return (
-    <div className="player" style={{
+    <div className="player" 
+    ref={playerRef} style={{
       position: 'absolute',
       top: position.y,
       left: position.x,
